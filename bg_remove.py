ef6c9ea17975e0f5926df671862158e0816c7654
GabrielLDN <gaellopes@protonmail.com>
2025-03-02 17:40:28 -0300

    style: reformat file uploader line for improved readability (E501)

--- bg_remove.py
+++ bg_remove.py
@@ -47,7 +47,9 @@ def fix_image(upload):
 
 
 col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+my_upload = st.sidebar.file_uploader(
+    "Upload an image", type=["png", "jpg", "jpeg"]
+)
 
 if my_upload is not None:
     if my_upload.size > MAX_FILE_SIZE:
