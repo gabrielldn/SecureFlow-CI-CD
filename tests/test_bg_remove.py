32d5e0eff98da1e1c525e92e0f337203302d78b8
GabrielLDN <gaellopes@protonmail.com>
2025-03-02 17:47:51 -0300

    test: fix module import error by updating PYTHONPATH in test_bg_remove.py

--- tests/test_bg_remove.py
+++ tests/test_bg_remove.py
@@ -1,6 +1,11 @@
-import io
-from PIL import Image
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from bg_remove import convert_image
+from PIL import Image
+import io
 
 def test_convert_image():
     img = Image.new("RGB", (100, 100), color="red")
