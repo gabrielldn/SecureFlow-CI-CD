668bdac26e961a5d7d7cb69e4ae680a59eee283b
GabrielLDN <gaellopes@protonmail.com>
2025-03-02 17:37:31 -0300

    style: reformatar bg_remove.py para conformidade com PEP8

--- bg_remove.py
+++ bg_remove.py
@@ -4,19 +4,25 @@ from rembg import remove
 from PIL import Image
 from io import BytesIO
 
-st.set_page_config(layout="wide", page_title="Image Background Remover")
+
+st.set_page_config(
+    layout="wide", page_title="Image Background Remover"
+)
 
 st.write("## Remove background from your image")
 desc = (
-    ":dog: Try uploading an image to watch the background magically removed. "
-    "Full quality images can be downloaded from the sidebar. This code is open source and "
-    "available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. "
-    "Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    ":dog: Try uploading an image to watch the background magically "
+    "removed. Full quality images can be downloaded from the sidebar. "
+    "This code is open source and available [here]"
+    "(https://github.com/gabrielldn/SecureFlow-CI-CD) on GitHub. "
+    "Special thanks to the [rembg library]"
+    "(https://github.com/danielgatis/rembg) :grin:"
 )
 st.write(textwrap.fill(desc, width=70))
 st.sidebar.write("## Upload and download :gear:")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
 
 # Download the fixed image
 def convert_image(img):
@@ -24,6 +30,7 @@ def convert_image(img):
     img.save(buf, format="PNG")
     byte_im = buf.getvalue()
     return byte_im
+
 
 def fix_image(upload):
     image = Image.open(upload)
@@ -34,14 +41,20 @@ def fix_image(upload):
     col2.write("Fixed Image :wrench:")
     col2.image(fixed)
     st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
+    st.sidebar.download_button(
+        "Download fixed image", convert_image(fixed), "fixed.png", "image/png"
+    )
+
 
 col1, col2 = st.columns(2)
 my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 if my_upload is not None:
     if my_upload.size > MAX_FILE_SIZE:
-        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+        st.error(
+            "The uploaded file is too large. Please upload an image smaller "
+            "than 5MB."
+        )
     else:
         fix_image(upload=my_upload)
 else:
